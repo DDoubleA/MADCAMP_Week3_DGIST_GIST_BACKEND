d2a249acf0e716bfb388ca275b142cd183759ee7
DDoubleA <ace9804@unist.ac.kr>
2022-01-14 21:14:25 +0900

    gotoserver

--- index.js
+++ index.js
@@ -1,21 +1,63 @@
 const express = require('express')
 const router = express.Router()
 const app = express()
-const unist = require('.//router/unist')
 const dgist = require('.//router/dgist')
-const gist = require('.//router/gist')
+const mongoose = require('mongoose')
 
 
-app.use('/unist', unist)
-app.use('/dgist', dgist)
-app.use('/gist', gist)
-app.get('/', function(req, res) {
-    res.send('hello here is home page')
-})
-
-const port = 8080
+const port = 443
+mongoose.connect('mongodb+srv://junyoung:junyoung@tweet.frfvm.mongodb.net/dgist?retryWrites=true&w=majority', {
+  }).then(console.log('DB is connected'));
 app.listen(port, () =>{
     console.log(`Server listening at %d`,port)
 })
 
+app.use('/dgist', dgist)
 
+app.get('/', function(req, res) {
+    res.send('hello here is home page')
+})
+
+
+
+
+const {Schema} = mongoose
+
+const UserSchema = new Schema({
+    UserID : String,
+    taken : [{과목번호: String,
+            교과목명:  String,
+            이수구분: String,
+            교과분야: String,
+            교과영역: String,
+            학위구분: String,
+            학점    : String,
+            비고    :String
+        }]
+    
+})
+
+const user = mongoose.model('users',UserSchema);
+
+function getByuserId(userId) {
+    return user.find({'UserID' : userId }).then((result) => {
+            console.log(result)
+            return result})
+}
+function getByall() {
+    return user.find();
+}
+
+    
+
+var newuser = new user({
+    UserID : "최준영",
+    taken :[{과목번호: 'cse496', 교과목명: '몰입캠프'},{과목번호 : 'cse234', 교과목명: '간다'}]
+});
+// newuser.save(function(err, newuser){
+//     if(err) return console.error(err);
+//     // console.dir(newuser.body);
+// });
+
+
+// console.log(user.find())
