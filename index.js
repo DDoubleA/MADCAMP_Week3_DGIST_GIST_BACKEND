77f4f482fe354ba91a7bd2ece3c9ef38276400d7
root <ace9804@unist.ac.kr>
2022-01-15 11:09:40 +0000

    post, get

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const express = require('express')
 const router = express.Router()
 const app = express()
 const dgist = require('.//router/dgist')
+const users = require('.//router/users')
+const cors = require('cors');
 const mongoose = require('mongoose')
+const { colours } = require('nodemon/lib/config/defaults')
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
 const port = 443
 mongoose.connect('mongodb+srv://junyoung:junyoung@tweet.frfvm.mongodb.net/dgist?retryWrites=true&w=majority', {
   }).then(console.log('DB is connected'));
 app.listen(port, () =>{
     console.log(`Server listening at %d`,port)
-})
-
-app.use('/dgist', dgist)
-
-app.get('/', function(req, res) {
-    res.send('hello here is home page')
 })
 
 
@@ -25,35 +26,69 @@ const {Schema} = mongoose
 
 const UserSchema = new Schema({
     UserID : String,
+    PassWord : String,
     taken : [{과목번호: String,
             교과목명:  String,
             이수구분: String,
             교과분야: String,
             교과영역: String,
             학위구분: String,
-            학점    : String,
+            학점    : Number,
             비고    :String
         }]
     
 })
 
 const user = mongoose.model('users',UserSchema);
-
-function getByuserId(userId) {
-    return user.find({'UserID' : userId }).then((result) => {
-            console.log(result)
-            return result})
-}
-function getByall() {
-    return user.find();
-}
-
     
+app.use('/dgist', dgist)
+app.use('/users', users)
+app.get('/', function(req, res) {
+    console.log("hello")
+})
 
-var newuser = new user({
-    UserID : "최준영",
-    taken :[{과목번호: 'cse496', 교과목명: '몰입캠프'},{과목번호 : 'cse234', 교과목명: '간다'}]
-});
+//제출하기
+app.post('/user', function(req, res) {
+    let userId = req.body.userid
+    let taken = req.body.isheard
+    console.log(userId);
+    user.findOne({UserID: userId}).then((doc) =>{
+        if(doc == null) { //save
+            const newuser = new user({
+                UserID : userId,
+                taken : taken
+            })
+            newuser.save().then(() => console.log('Saved successfully'));
+        } else {// edit
+            user.updateOne({_id: doc._id}, {$set : {taken: taken}}).then(console.log('updated'))
+        }
+    })
+   
+})
+//courses 정보 요청 
+app.get('/user/:id', function(req, res){
+    console.log("here")
+    // console.log(req.params.id)
+    let id = req.params.id
+    user.findOne({'UserID':id}).then((result=> {
+        console.log(result.UserID)
+        res.json(result.taken)
+    }))
+    
+})
+
+//로그인 요청
+app.post('/login', function(req,res) {
+    console.log("login post", req.body)
+    // console.log(req.body.pw)
+})
+//회원가입 요청
+app.post('/register', function(req,res){
+    console.log("register post ", req.body)
+
+})
+
+
 // newuser.save(function(err, newuser){
 //     if(err) return console.error(err);
 //     // console.dir(newuser.body);
--- router/users.js
+++ router/users.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 router.get('/', (req, res)=>{
     // res.send('hello dgist');
-    console.log("here is dgist")
+    console.log("here is userpage")
 });
+
+router.post('/', (req,res)=>{
+    const id = req.body.id
+    const pw = req.body.pw
+    console.log(id, pw)
+})
 
 module.exports = router;
