b377b46fc1506fd5f7b9f0092567507f17bf331e
root <ace9804@unist.ac.kr>
2022-01-17 09:19:14 +0000

    before add gist

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const {Schema} = mongoose
 const UserSchema = new Schema({
     UserID : String,
     PassWord : String,
+    SI : Number,
     taken : [{과목번호: String,
             교과목명:  String,
             이수구분: String,
@@ -51,30 +52,22 @@ app.post('/user', function(req, res) {
     // console.log("dasssssssssssssssssssssssssssssssssssssssssssssssssssssssssss");
     let userId = req.body.userid.userId
     let taken = req.body.taken.isheard
+    let SI = req.body.SI.SI
     // console.log(userId);
-    // console.log(taken);
+    // console.log(taken);asd
     user.findOne({UserID: userId}).then((doc) =>{
-        if(doc == null) { //save
-            const newuser = new user({
-                UserID : userId,
-                taken : taken
-            })
-            newuser.save().then(() => console.log('Saved successfully'));
-        } else {// edit
-            user.updateOne({_id: doc._id}, {$push : {taken: taken}}).then(console.log('updated'))
-        }
+        // edit
+        user.updateOne({_id: doc._id}, {$set : {taken: taken, SI: SI}}).then(console.log('updated'))
     })
    
 })
 //courses 정보 요청 
 app.get('/user/:id', function(req, res){
-    // console.log("here")
-    // console.log(req.params.id)
     let id = req.params.id
 
     user.findOne({'UserID':id}).then((result=> {
         console.log(result.UserID)
-        res.json(result.taken)
+        res.json({taken: result.taken, SI: result.SI})
     }))
     
 })
@@ -88,10 +81,11 @@ app.post('/login', function(req,res) {
         if(doc == null) { //save
             res.send(`There is no registered ID`)
         } else {// edit
-            // console.log(`doc.PassWord`, doc.PassWord)
-            // console.log(`PassWord`, PassWord)
-            if (doc.PassWord == PassWord){
-                res.send(`correct`)
+            if(doc.taken.length == 0){
+                res.send(`correct`) // new user
+            }
+            else if (doc.PassWord == PassWord){
+                res.send(`courses`) // login
             }
             else{
                 res.send(`Wrong Password`)
@@ -99,18 +93,19 @@ app.post('/login', function(req,res) {
         }
     })
 
-    // console.log(req.body.pw)
 })
+
 //회원가입 요청
 app.post('/register', function(req,res){
-    console.log("register post ", req.body)
+    // console.log("register post ", req.body)
     let userId = req.body.id
     let PassWord = req.body.pw 
     user.findOne({UserID: userId}).then((doc) =>{
         if(doc == null) { //save
             const newuser = new user({
                 UserID : userId,
-                PassWord : PassWord
+                PassWord : PassWord,
+                SI : 0
             })
             newuser.save().then(() => console.log('Saved successfully'));
             res.send(`registered successfully`)
@@ -121,11 +116,30 @@ app.post('/register', function(req,res){
 
 })
 
+app.post('/kakao', function(req,res) {
+    console.log("login post", req.body)
+    let userId =req.body.id
+    let PassWord = req.body.pw 
+    user.findOne({UserID: userId}).then((doc) =>{
+        if(doc == null) { //save
+            const newuser = new user({
+                UserID : userId,
+                PassWord : PassWord,
+                SI : 0
+            })
+            newuser.save().then(() => console.log('Saved successfully from kakao'))
+            res.send(`correct`)
+        }else {// edit
+            if(doc.taken.length == 0){
+                res.send(`correct`) // go to survey
+            }
+            else{
+                res.send(`courses`) // go to result
+            }
+        }
+    })
 
-// newuser.save(function(err, newuser){
-//     if(err) return console.error(err);
-//     // console.dir(newuser.body);
-// });
+})
 
 
-// console.log(user.find())
+
