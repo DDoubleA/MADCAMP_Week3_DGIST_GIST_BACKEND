e060a6c662cc82828c58a3f7db426f1fab2a7181
root <ace9804@unist.ac.kr>
2022-01-18 07:47:17 +0000

    finish

--- index.js
+++ index.js
@@ -28,6 +28,11 @@ const UserSchema = new Schema({
     UserID : String,
     PassWord : String,
     SI : Number,
+    School : String,
+    Major : {전공타입 : String,
+            전공명1 : String,
+            전공명2 : String},
+
     taken : [{과목번호: String,
             교과목명:  String,
             이수구분: String,
@@ -47,29 +52,28 @@ app.get('/', function(req, res) {
     // console.log("hello")
 })
 
-//제출하기
+//제출하기 Course.jsx/ handleSubmit => {userid : {userId} , taken : {isheard}, SI: {SI}, School: {School}, Major : {Major}}
 app.post('/user', function(req, res) {
-    // console.log("dasssssssssssssssssssssssssssssssssssssssssssssssssssssssssss");
+    // console.log(`req.body`, req.body)
     let userId = req.body.userid.userId
     let taken = req.body.taken.isheard
     let SI = req.body.SI.SI
-    // console.log(userId);
-    // console.log(taken);asd
+    let School = req.body.School
+    let Major = req.body.Major.major
     user.findOne({UserID: userId}).then((doc) =>{
-        // edit
-        user.updateOne({_id: doc._id}, {$set : {taken: taken, SI: SI}}).then(console.log('updated'))
+        // edit information
+        user.updateOne({_id: doc._id}, {$set : {taken: taken, SI: SI, School: School, Major : Major}}).then(console.log(userId,' submitted courses'))
     })
    
 })
+
 //courses 정보 요청 
 app.get('/user/:id', function(req, res){
     let id = req.params.id
-
     user.findOne({'UserID':id}).then((result=> {
-        console.log(result.UserID)
-        res.json({taken: result.taken, SI: result.SI})
+        console.log(result.UserID, "requested taken Courses")
+        res.json({taken: result.taken, SI: result.SI, Major: result.Major})
     }))
-    
 })
 
 //로그인 요청
@@ -78,21 +82,29 @@ app.post('/login', function(req,res) {
     let userId =req.body.id
     let PassWord = req.body.pw 
     user.findOne({UserID: userId}).then((doc) =>{
-        if(doc == null) { //save
+        if(doc == null) { //DB에 user 정복 없을 때 로그인 불가
             res.send(`There is no registered ID`)
-        } else {// edit
-            if(doc.taken.length == 0){
+        } else {
+            if(doc.taken.length == 0){ // taken Course 정보가 없으면 Survey로
                 res.send(`correct`) // new user
             }
-            else if (doc.PassWord == PassWord){
-                res.send(`courses`) // login
+            else if (doc.PassWord == PassWord){// taken Course 정보가 있고 db password와 일치하면 로그인
+                // res.send(`courses`) // 기존 이동
+                if(doc.School === "dgist"){
+                    res.send(`dgist`)
+                }
+                else if(doc.School === "gist"){
+                    res.send(`gist`)
+                }
+                else( // 기존 학교 없을 때 가입한 유저?
+                    res.send(`courses`)
+                )
             }
-            else{
+            else{ // 틀리면 로그인 불가
                 res.send(`Wrong Password`)
             }
         }
     })
-
 })
 
 //회원가입 요청
@@ -107,7 +119,7 @@ app.post('/register', function(req,res){
                 PassWord : PassWord,
                 SI : 0
             })
-            newuser.save().then(() => console.log('Saved successfully'));
+            newuser.save().then(() => console.log(userId, 'registered successfully'));
             res.send(`registered successfully`)
         } else {// edit
             res.send(`duplicated ID`)
